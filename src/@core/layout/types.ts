12f4bb98afbb48d38250b1e477bd4d2862b7f294
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2021-12-06 15:29:35 +0530

    feat(types): add some types
    Add types for things that are in customizer

---
+++ src/@core/layout/types.ts
@@ -0,0 +1,17 @@
+export type Layout = 'vertical' | 'horizontal' | 'blank' | 'blankWithAppBar'
+
+export type Skin = 'default' | 'bordered' | 'semi-dark'
+
+export type Mode = 'light' | 'dark'
+
+export type ContentWidth = 'full' | 'boxed'
+
+export type Direction = 'ltr' | 'rtl'
+
+export type AppBar = 'fixed' | 'static' | 'hidden'
+
+export type Footer = 'fixed' | 'static' | 'hidden'
+
+export type ThemeColor = 'primary' | 'secondary' | 'error' | 'warning' | 'info' | 'success'
+
+export type VerticalNavToggle = 'accordion' | 'collapse'
