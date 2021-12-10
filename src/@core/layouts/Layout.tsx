2622cb817e0a59a82a9805ea9022df7898fd2c52
Vrushank Brahmakshatriya <vrushankpb@gmail.com>
2021-12-10 20:41:37 +0530

    feat(layout): layout props
    defined layout props

--- src/@core/layouts/Layout.tsx
+++ src/@core/layouts/Layout.tsx
@@ -1,9 +1,12 @@
 // ** React Imports
-import { FC, ReactNode } from 'react'
+import { FC } from 'react'
 
 // ** MUI Imports
 import { Theme } from '@mui/material/styles'
 import useMediaQuery from '@mui/material/useMediaQuery'
+
+// ** Type Import
+import { LayoutProps } from '@core/layouts/types'
 
 // ** Layout Components
 import VerticalLayout from './VerticalLayout'
@@ -12,20 +15,26 @@ import HorizontalLayout from './HorizontalLayout'
 // ** Hook Import
 import { useSettings } from '@core/hooks/useSettings'
 
-interface Props {
-  children: ReactNode
-}
+const Layout: FC<LayoutProps> = (props: LayoutProps) => {
+  const { children } = props
 
-const Layout: FC<Props> = ({ children }: Props) => {
   // ** Hooks
   const { settings } = useSettings()
   const hidden = useMediaQuery((theme: Theme) => theme.breakpoints.down('lg'))
 
   if (settings.layout === 'horizontal') {
-    return <HorizontalLayout hidden={hidden}>{children}</HorizontalLayout>
+    return (
+      <HorizontalLayout hidden={hidden} {...props}>
+        {children}
+      </HorizontalLayout>
+    )
   }
 
-  return <VerticalLayout hidden={hidden}>{children}</VerticalLayout>
+  return (
+    <VerticalLayout hidden={hidden} {...props}>
+      {children}
+    </VerticalLayout>
+  )
 }
 
 export default Layout
