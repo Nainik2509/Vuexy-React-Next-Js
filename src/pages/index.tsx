9bc771172ad6997acf33e189b03e4b09beb1c355
Vrushank Brahmakshatriya <vrushankpb@gmail.com>
2021-12-21 17:14:35 +0530

    feat(props): getStaticProps axios
    Working example of axios inside get static props and using it in component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
-// ** React Imports
-import { FC } from 'react'
+// ** Next Imports
+import { NextPage } from 'next'
 
 // ** MUI Imports
 import Typography from '@mui/material/Typography'
 
-const Home: FC = () => {
+// ** Third Party Imports
+import axios from 'axios'
+
+const Home: NextPage = ({ navData }: any) => {
+  console.log(`navData`, navData)
+
   return (
     <div>
       <Typography variant='h5' sx={{ mb: 2 }}>
@@ -58,6 +63,18 @@ const Home: FC = () => {
       <Typography>Typography</Typography>
     </div>
   )
+}
+
+// This function gets called at build time
+export async function getStaticProps() {
+  const res = await axios.get('/navigation/data')
+  const navData = res.data
+
+  return {
+    props: {
+      navData
+    }
+  }
 }
 
 export default Home
