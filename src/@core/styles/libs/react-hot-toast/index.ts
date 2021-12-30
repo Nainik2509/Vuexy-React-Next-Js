88f4282743a8695f3365dcabfe964bfcc5b87da5
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2021-12-30 17:18:27 +0530

    refactor(multi): code refactor
    Refactor card-snippet file
    Fix code styling

--- src/@core/styles/libs/react-hot-toast/index.ts
+++ src/@core/styles/libs/react-hot-toast/index.ts
@@ -5,6 +5,7 @@ import Box, { BoxProps } from '@mui/material/Box'
 const ReactHotToast = styled(Box)<BoxProps>(({ theme }) => ({
   '& .react-hot-toast': {
     boxShadow: theme.shadows[3],
+    zIndex: theme.zIndex.snackbar,
     color: theme.palette.text.primary,
     background: theme.palette.background.default
   }
