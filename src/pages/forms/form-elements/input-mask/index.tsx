2539f13b3bde779caa5545f14c59eab969343bcb
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2022-01-11 16:40:15 +0530

    refactor(input-mask): refactor code
    Move input mask styling wrapper to respective pages' index files

--- src/pages/forms/form-elements/input-mask/index.tsx
+++ src/pages/forms/form-elements/input-mask/index.tsx
@@ -7,6 +7,9 @@ import Typography from '@mui/material/Typography'
 import PageHeader from 'src/@core/components/page-header'
 import CardSnippet from 'src/@core/components/card-snippet'
 
+// ** Styled Component
+import CleaveWrapper from 'src/@core/styles/libs/react-cleave'
+
 // ** Example Import
 import InputMaskExamples from 'src/views/forms/form-elements/input-mask/InputMaskExamples'
 
@@ -15,23 +18,25 @@ import * as source from 'src/views/forms/form-elements/input-mask/InputMaskSourc
 
 const InputMask = () => {
   return (
-    <Grid container spacing={6}>
-      <PageHeader
-        title={
-          <Typography variant='h5'>
-            <Link href='https://github.com/nosir/cleave.js' target='_blank'>
-              Cleave.js
-            </Link>
-          </Typography>
-        }
-        subtitle={<Typography variant='body2'>Format input text content when you are typing</Typography>}
-      />
-      <Grid item xs={12}>
-        <CardSnippet title='Input Masks' code={source.InputMaskExamplesCode}>
-          <InputMaskExamples />
-        </CardSnippet>
+    <CleaveWrapper>
+      <Grid container spacing={6}>
+        <PageHeader
+          title={
+            <Typography variant='h5'>
+              <Link href='https://github.com/nosir/cleave.js' target='_blank'>
+                Cleave.js
+              </Link>
+            </Typography>
+          }
+          subtitle={<Typography variant='body2'>Format input text content when you are typing</Typography>}
+        />
+        <Grid item xs={12}>
+          <CardSnippet title='Input Masks' code={source.InputMaskExamplesCode}>
+            <InputMaskExamples />
+          </CardSnippet>
+        </Grid>
       </Grid>
-    </Grid>
+    </CleaveWrapper>
   )
 }
 
--- src/views/forms/form-elements/input-mask/InputMaskSourceCode.tsx
+++ src/views/forms/form-elements/input-mask/InputMaskSourceCode.tsx
@@ -1,13 +1,12 @@
-export const InputMaskExamplesCode = (<pre className='language-jsx'><code className='language-jsx'>{`// ** MUI Imports
+export const InputMaskExamplesCode = (
+  <pre className='language-jsx'>
+    <code className='language-jsx'>{`// ** MUI Imports
 import Grid from '@mui/material/Grid'
 import InputLabel from '@mui/material/InputLabel'
 
 // ** CleaveJS Imports
 import Cleave from 'cleave.js/react'
 import 'cleave.js/dist/addons/cleave-phone.us'
-
-// ** Styled component
-import CleaveWrapper from 'src/@core/styles/libs/react-cleave'
 
 const InputMaskExamples = () => {
   return (
@@ -16,93 +15,73 @@ const InputMaskExamples = () => {
         <InputLabel htmlFor='credit-card' sx={{ mb: 2, fontSize: '.75rem' }}>
           Credit Card
         </InputLabel>
-        <CleaveWrapper>
-          <Cleave id='credit-card' options={{ creditCard: true }} placeholder='0000 0000 0000 0000' />
-        </CleaveWrapper>
+        <Cleave id='credit-card' options={{ creditCard: true }} placeholder='0000 0000 0000 0000' />
       </Grid>
       <Grid item xs={12} sm={6} lg={4}>
         <InputLabel htmlFor='phone-number' sx={{ mb: 2, fontSize: '.75rem' }}>
           Phone Number
         </InputLabel>
-        <CleaveWrapper>
-          <Cleave id='phone-number' placeholder='1 234 567 8900' options={{ phone: true, phoneRegionCode: 'US' }} />
-        </CleaveWrapper>
+        <Cleave id='phone-number' placeholder='1 234 567 8900' options={{ phone: true, phoneRegionCode: 'US' }} />
       </Grid>
       <Grid item xs={12} sm={6} lg={4}>
         <InputLabel htmlFor='date' sx={{ mb: 2, fontSize: '.75rem' }}>
           Date
         </InputLabel>
-        <CleaveWrapper>
-          <Cleave
-            id='date'
-            placeholder='2001-01-01'
-            options={{ date: true, delimiter: '-', datePattern: ['Y', 'm', 'd'] }}
-          />
-        </CleaveWrapper>
+        <Cleave
+          id='date'
+          placeholder='2001-01-01'
+          options={{ date: true, delimiter: '-', datePattern: ['Y', 'm', 'd'] }}
+        />
       </Grid>
       <Grid item xs={12} sm={6} lg={4}>
         <InputLabel htmlFor='time' sx={{ mb: 2, fontSize: '.75rem' }}>
           Time
         </InputLabel>
-        <CleaveWrapper>
-          <Cleave id='time' placeholder='12:00:00' options={{ time: true, timePattern: ['h', 'm', 's'] }} />
-        </CleaveWrapper>
+        <Cleave id='time' placeholder='12:00:00' options={{ time: true, timePattern: ['h', 'm', 's'] }} />
       </Grid>
       <Grid item xs={12} sm={6} lg={4}>
         <InputLabel htmlFor='numeral' sx={{ mb: 2, fontSize: '.75rem' }}>
           Numeral
         </InputLabel>
-        <CleaveWrapper>
-          <Cleave
-            id='numeral'
-            placeholder='10,000'
-            options={{ numeral: true, numeralThousandsGroupStyle: 'thousand' }}
-          />
-        </CleaveWrapper>
+        <Cleave id='numeral' placeholder='10,000' options={{ numeral: true, numeralThousandsGroupStyle: 'thousand' }} />
       </Grid>
       <Grid item xs={12} sm={6} lg={4}>
         <InputLabel htmlFor='blocks' sx={{ mb: 2, fontSize: '.75rem' }}>
           Blocks
         </InputLabel>
-        <CleaveWrapper>
-          <Cleave id='blocks' placeholder='Blocks [4, 3, 3]' options={{ blocks: [4, 3, 3], uppercase: true }} />
-        </CleaveWrapper>
+        <Cleave id='blocks' placeholder='Blocks [4, 3, 3]' options={{ blocks: [4, 3, 3], uppercase: true }} />
       </Grid>
       <Grid item xs={12} sm={6} lg={4}>
         <InputLabel htmlFor='delimiter' sx={{ mb: 2, fontSize: '.75rem' }}>
           Delimiter
         </InputLabel>
-        <CleaveWrapper>
-          <Cleave
-            id='delimiter'
-            placeholder="Delimiter: '.'"
-            options={{ delimiter: '·', blocks: [3, 3, 3], uppercase: true }}
-          />
-        </CleaveWrapper>
+        <Cleave
+          id='delimiter'
+          placeholder="Delimiter: '.'"
+          options={{ delimiter: '·', blocks: [3, 3, 3], uppercase: true }}
+        />
       </Grid>
       <Grid item xs={12} sm={6} lg={4}>
         <InputLabel htmlFor='custom-delimiter' sx={{ mb: 2, fontSize: '.75rem' }}>
           Custom Delimiters
         </InputLabel>
-        <CleaveWrapper>
-          <Cleave
-            id='custom-delimiter'
-            placeholder="Delimiter: ['.', '.', '-']"
-            options={{ delimiters: ['.', '.', '-'], blocks: [3, 3, 3, 2], uppercase: true }}
-          />
-        </CleaveWrapper>
+        <Cleave
+          id='custom-delimiter'
+          placeholder="Delimiter: ['.', '.', '-']"
+          options={{ delimiters: ['.', '.', '-'], blocks: [3, 3, 3, 2], uppercase: true }}
+        />
       </Grid>
       <Grid item xs={12} sm={6} lg={4}>
         <InputLabel htmlFor='prefix' sx={{ mb: 2, fontSize: '.75rem' }}>
           Prefix
         </InputLabel>
-        <CleaveWrapper>
-          <Cleave id='prefix' options={{ prefix: '+63', blocks: [3, 3, 3, 4], uppercase: true }} />
-        </CleaveWrapper>
+        <Cleave id='prefix' options={{ prefix: '+63', blocks: [3, 3, 3, 4], uppercase: true }} />
       </Grid>
     </Grid>
   )
 }
 
 export default InputMaskExamples
-`}</code></pre>) 
+`}</code>
+  </pre>
+)
