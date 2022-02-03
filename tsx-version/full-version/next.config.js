7f952e83892e0afef4967d94fa1902aa269bd923
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2022-02-03 17:01:36 +0530

    remove(calendar): remove some fullcalendar packages from next.config

--- tsx-version/full-version/next.config.js
+++ tsx-version/full-version/next.config.js
@@ -2,12 +2,10 @@
 
 // Remove this if you're not using Fullcalendar features
 const withTM = require('next-transpile-modules')([
-  '@fullcalendar/common',
   '@fullcalendar/react',
   '@fullcalendar/daygrid',
   '@fullcalendar/list',
-  '@fullcalendar/timegrid',
-  '@fullcalendar/timeline'
+  '@fullcalendar/timegrid'
 ])
 
 module.exports = withTM({
