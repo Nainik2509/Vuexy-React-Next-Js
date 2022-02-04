a9b3328c4c6da2ab7ad25838d1aa43f9ffa0a2d9
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2022-02-04 16:37:52 +0530

    fix(calendar): fix error by adding back some packages

--- tsx-version/full-version/next.config.js
+++ tsx-version/full-version/next.config.js
@@ -2,6 +2,7 @@
 
 // Remove this if you're not using Fullcalendar features
 const withTM = require('next-transpile-modules')([
+  '@fullcalendar/common',
   '@fullcalendar/react',
   '@fullcalendar/daygrid',
   '@fullcalendar/list',
