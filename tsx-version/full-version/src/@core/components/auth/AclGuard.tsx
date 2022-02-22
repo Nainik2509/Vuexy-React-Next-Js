45a3cbcd7a24b80ca078229cd02948f74c2a93de
Vrushank Brahmakshatriya <vrushankpb@gmail.com>
2022-02-22 17:44:15 +0530

    fix(acl): fixed acl build ability
    fixed acl speed issue due to ability built each time new route is visited using state

--- tsx-version/full-version/src/@core/components/auth/AclGuard.tsx
+++ tsx-version/full-version/src/@core/components/auth/AclGuard.tsx
@@ -1,11 +1,11 @@
 // ** React Imports
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 
 // ** Next Imports
 import { useRouter } from 'next/router'
 
 // ** Types
-import type { ACLObj } from 'src/configs/acl'
+import type { ACLObj, AppAbility } from 'src/configs/acl'
 
 // ** Context Imports
 import { AbilityContext } from 'src/layouts/components/acl/Can'
@@ -30,11 +30,11 @@ const AclGuard = (props: AclGuardProps) => {
   // ** Props
   const { aclAbilities, children, guestGuard } = props
 
+  const [ability, setAbility] = useState<AppAbility | undefined>(undefined)
+
   // ** Hooks
   const auth = useAuth()
   const router = useRouter()
-
-  let ability
 
   // If guestGuard is true and user is not logged in or its an error page, render the page without checking access
   if (guestGuard || router.route === '/404' || router.route === '/500' || router.route === '/') {
@@ -42,8 +42,8 @@ const AclGuard = (props: AclGuardProps) => {
   }
 
   // User is logged in, build ability for the user based on his role
-  if (auth.user && auth.user.role) {
-    ability = buildAbilityFor(auth.user.role, aclAbilities.subject)
+  if (auth.user && auth.user.role && !ability) {
+    setAbility(buildAbilityFor(auth.user.role, aclAbilities.subject))
   }
 
   // Check the access of current user and render pages
