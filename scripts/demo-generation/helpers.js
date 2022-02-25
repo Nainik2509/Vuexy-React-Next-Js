f9ead2ff85e823e10a26e944995bcd10bab8e585
ronaklalwaniii <ronak@pixinvent.com>
2022-02-25 18:46:59 +0530

    refactor(scripts): refactor demos scripts

--- scripts/demo-generation/helpers.js
+++ scripts/demo-generation/helpers.js
@@ -3,15 +3,19 @@ const path = require('path')
 const pathConfig = require('../configs/paths.json')
 
 const copyDirectory = (source, destination) => {
-    fs.mkdirSync(destination, { recursive: true })
+    fs.mkdirSync(destination, {
+        recursive: true
+    })
 
-    fs.readdirSync(source, { withFileTypes: true }).forEach((entry) => {
+    fs.readdirSync(source, {
+        withFileTypes: true
+    }).forEach((entry) => {
         let sourcePath = path.join(source, entry.name);
         let destinationPath = path.join(destination, entry.name);
 
-        entry.isDirectory()
-            ? copyDirectory(sourcePath, destinationPath)
-            : fs.copyFileSync(sourcePath, destinationPath);
+        entry.isDirectory() ?
+            copyDirectory(sourcePath, destinationPath) :
+            fs.copyFileSync(sourcePath, destinationPath);
     });
 }
 
@@ -34,52 +38,51 @@ const testFoldersToModify = [
         to: `./temp-folder/${pathConfig.fullVersionTSXPath.replace('../../', '')}/src/pages/forms/form-elements/test`,
     },
 
-    // {
-    //     from: `${pathConfig.fullVersionJSXPath}/src/views/components/test`,
-    //     to: `./temp-folder/${pathConfig.fullVersionJSXPath.replace('../../', '')}/src/views/components/test`,
-    // },
-    // {
-    //     from: `${pathConfig.fullVersionJSXPath}/src/pages/components/test`,
-    //     to: `./temp-folder/${pathConfig.fullVersionJSXPath.replace('../../', '')}/src/pages/components/test`,
-    // },
-    // {
-    //     from: `${pathConfig.fullVersionJSXPath}/src/views/forms/form-elements/test`,
-    //     to: `./temp-folder/${pathConfig.fullVersionJSXPath.replace('../../', '')}/src/views/forms/form-elements/test`,
-    // }
-    // {
-    //     from: `${pathConfig.fullVersionJSXPath}/src/pages/forms/form-elements/test`,
-    //     to: `./temp-folder/${pathConfig.fullVersionJSXPath.replace('../../', '')}/src/pages/forms/form-elements/test`,
-    // }
+    {
+        from: `${pathConfig.fullVersionJSXPath}/src/views/components/test`,
+        to: `./temp-folder/${pathConfig.fullVersionJSXPath.replace('../../', '')}/src/views/components/test`,
+    },
+    {
+        from: `${pathConfig.fullVersionJSXPath}/src/pages/components/test`,
+        to: `./temp-folder/${pathConfig.fullVersionJSXPath.replace('../../', '')}/src/pages/components/test`,
+    },
+    {
+        from: `${pathConfig.fullVersionJSXPath}/src/views/forms/form-elements/test`,
+        to: `./temp-folder/${pathConfig.fullVersionJSXPath.replace('../../', '')}/src/views/forms/form-elements/test`,
+    },
+    {
+        from: `${pathConfig.fullVersionJSXPath}/src/pages/forms/form-elements/test`,
+        to: `./temp-folder/${pathConfig.fullVersionJSXPath.replace('../../', '')}/src/pages/forms/form-elements/test`,
+    }
 ]
 
-const testFoldersToCopy = [
-{
-    from: `${pathConfig.fullVersionTSXPath}/src/@fake-db/server-side-menu`,
-    to: `./temp-folder/${pathConfig.fullVersionTSXPath.replace('../../', '')}/src/@fake-db/server-side-menu`,
-},
-{
-    from: `${pathConfig.fullVersionTSXPath}/src/navigation`,
-    to: `./temp-folder/${pathConfig.fullVersionTSXPath.replace('../../', '')}/src/navigation`,
-},
-    // {
-    //     from: `${pathConfig.fullVersionJSXPath}/src/@fake-db/server-side-menu`,
-    //     to: `./temp-folder/${pathConfig.fullVersionJSXPath.replace('../../', '')}/src/views/components/test`,
-    // },
-    // {
-    //     from: `${pathConfig.fullVersionJSXPath}/src/navigation`,
-    //     to: `./temp-folder/${pathConfig.fullVersionJSXPath.replace('../../', '')}/src/views/components/test`,
-    // },
+const testFoldersToCopy = [{
+        from: `${pathConfig.fullVersionTSXPath}/src/@fake-db/server-side-menu`,
+        to: `./temp-folder/${pathConfig.fullVersionTSXPath.replace('../../', '')}/src/@fake-db/server-side-menu`,
+    },
+    {
+        from: `${pathConfig.fullVersionTSXPath}/src/navigation`,
+        to: `./temp-folder/${pathConfig.fullVersionTSXPath.replace('../../', '')}/src/navigation`,
+    },
+    {
+        from: `${pathConfig.fullVersionJSXPath}/src/@fake-db/server-side-menu`,
+        to: `./temp-folder/${pathConfig.fullVersionJSXPath.replace('../../', '')}/src/views/components/test`,
+    },
+    {
+        from: `${pathConfig.fullVersionJSXPath}/src/navigation`,
+        to: `./temp-folder/${pathConfig.fullVersionJSXPath.replace('../../', '')}/src/views/components/test`,
+    },
 ]
 
 const filesWithTestObj = [
     `${pathConfig.fullVersionTSXPath}/src/navigation/vertical/index.ts`,
     `${pathConfig.fullVersionTSXPath}/src/navigation/horizontal/index.ts`,
     `${pathConfig.fullVersionTSXPath}/src/@fake-db/server-side-menu/vertical.ts`,
-    `${pathConfig.fullVersionTSXPath}/src/@fake-db/server-side-menu/horizontal.ts`,
-    // `${pathConfig.fullVersionTSXPath}/src/navigation/vertical/index.js`,
-    // `${pathConfig.fullVersionTSXPath}/src/navigation/horizontal/index.js`,
-    // `${pathConfig.fullVersionJSXPath}/src/@fake-db/server-side-menu/vertical.js`,
-    // `${pathConfig.fullVersionJSXPath}/src/@fake-db/server-side-menu/horizontal.js`,
+    `${pathConfig.fullVersionTSXPath}/src/@fake-db/server-side-menu/horizontal.ts`,    
+    `${pathConfig.fullVersionJSXPath}/src/navigation/vertical/index.js`,
+    `${pathConfig.fullVersionJSXPath}/src/navigation/horizontal/index.js`,
+    `${pathConfig.fullVersionJSXPath}/src/@fake-db/server-side-menu/vertical.js`,
+    `${pathConfig.fullVersionJSXPath}/src/@fake-db/server-side-menu/horizontal.js`,
 ]
 
 module.exports = {
