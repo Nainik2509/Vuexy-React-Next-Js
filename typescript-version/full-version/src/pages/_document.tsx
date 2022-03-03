75ada70949de295cbe41b67ee96b06dab2427a92
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2022-03-03 18:05:28 +0530

    refactor(multi): code refactor
    Update react-hot-toast wrapper position
    Update favicon rel in _document.tsx file

--- typescript-version/full-version/src/pages/_document.tsx
+++ typescript-version/full-version/src/pages/_document.tsx
@@ -22,7 +22,7 @@ class CustomDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
           />
           <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
-          <link rel='icon' href='/favicon.ico' />
+          <link rel='shortcut icon' href='/favicon.ico' />
         </Head>
         <body>
           <Main />
