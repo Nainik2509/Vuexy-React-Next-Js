3214c0b5de093950cbf6a8f86597c91d166eac50
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2022-03-30 11:21:58 +0530

    refactor(multi): refactor multiple items
    Remove js-version, demo-configs & starter-kit and ignore them in git
    Update MUI packages
    Ignore documentation folder in git

--- javascript-version/full-version/src/pages/dashboards/analytics/index.js
+++
@@ -1,8 +0,0 @@
-// ** MUI Imports
-import Typography from '@mui/material/Typography'
-
-const AnalyticsDashboard = () => {
-  return <Typography variant='h4'>Analytics Dashboard</Typography>
-}
-
-export default AnalyticsDashboard
--- javascript-version/full-version/src/pages/dashboards/ecommerce/index.js
+++
@@ -1,8 +0,0 @@
-// ** MUI Imports
-import Typography from '@mui/material/Typography'
-
-const EcommerceDashboard = () => {
-  return <Typography variant='h4'>eCommerce Dashboard</Typography>
-}
-
-export default EcommerceDashboard
