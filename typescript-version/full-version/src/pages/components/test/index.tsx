4ed1dfa1aca79199a8e93108affc511978abdbad
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2022-03-31 19:18:38 +0530

    feat(transfer-list): add transfer-list in test page

--- typescript-version/full-version/src/pages/components/test/index.tsx
+++ typescript-version/full-version/src/pages/components/test/index.tsx
@@ -18,6 +18,8 @@ import OutsidePopover from 'src/views/components/test/OutsidePopover'
 import OutsideTooltip from 'src/views/components/test/OutsideTooltip'
 import OutsideProgress from 'src/views/components/test/OutsideProgress'
 import OutsideBreadcrumb from 'src/views/components/test/OutsideBreadcrumb'
+import TransferListBasic from 'src/views/components/test/TransferListBasic'
+import TransferListEnhanced from 'src/views/components/test/TransferListEnhanced'
 
 const Test = () => {
   return (
@@ -49,10 +51,10 @@ const Test = () => {
       <Grid item xs={12}>
         <Typography variant='h5'>Breadcrumb</Typography>
       </Grid>
-      <Grid item xs={12} sm={6}>
+      <Grid item xs={12} md={6}>
         <CardBreadcrumb />
       </Grid>
-      <Grid item xs={12} sm={6}>
+      <Grid item xs={12} md={6}>
         <OutsideBreadcrumb />
       </Grid>
       <Grid item xs={12}>
@@ -76,11 +78,23 @@ const Test = () => {
       <Grid item xs={12}>
         <Typography variant='h5'>AppBar</Typography>
       </Grid>
-      <Grid item xs={12} sm={6}>
+      <Grid item xs={12} md={6}>
         <CardAppBar />
       </Grid>
-      <Grid item xs={12} sm={6}>
+      <Grid item xs={12} md={6}>
         <OutsideAppBar />
+      </Grid>
+      <Grid item xs={12} lg={6}>
+        <Typography variant='h5'>Basic Transfer List</Typography>
+      </Grid>
+      <Grid item xs={12} lg={6}>
+        <Typography variant='h5'>Enhanced Transfer List</Typography>
+      </Grid>
+      <Grid item xs={12} lg={6}>
+        <TransferListBasic />
+      </Grid>
+      <Grid item xs={12} lg={6}>
+        <TransferListEnhanced />
       </Grid>
       <Grid item xs={12}>
         <Typography variant='h5'>Drawer</Typography>
