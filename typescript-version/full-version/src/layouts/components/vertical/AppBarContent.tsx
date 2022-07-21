1055393a40af3a22f476bde3af3709668507ed6c
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2022-07-21 17:45:42 +0530

    refactor(notification): get data from user
    Add notification count on the notification icon
    Fix some stylings

--- typescript-version/full-version/src/layouts/components/vertical/AppBarContent.tsx
+++ typescript-version/full-version/src/layouts/components/vertical/AppBarContent.tsx
@@ -13,7 +13,9 @@ import Autocomplete from 'src/layouts/components/Autocomplete'
 import ModeToggler from 'src/@core/layouts/components/shared-components/ModeToggler'
 import UserDropdown from 'src/@core/layouts/components/shared-components/UserDropdown'
 import LanguageDropdown from 'src/@core/layouts/components/shared-components/LanguageDropdown'
-import NotificationDropdown from 'src/@core/layouts/components/shared-components/NotificationDropdown'
+import NotificationDropdown, {
+  NotificationsType
+} from 'src/@core/layouts/components/shared-components/NotificationDropdown'
 
 interface Props {
   hidden: boolean
@@ -21,6 +23,51 @@ interface Props {
   toggleNavVisibility: () => void
   saveSettings: (values: Settings) => void
 }
+
+const notifications: NotificationsType[] = [
+  {
+    meta: 'Today',
+    avatarAlt: 'Flora',
+    title: 'Congratulation Flora! 🎉',
+    avatarImg: '/images/avatars/4.png',
+    subtitle: 'Won the monthly best seller badge'
+  },
+  {
+    meta: 'Yesterday',
+    avatarColor: 'primary',
+    subtitle: '5 hours ago',
+    avatarText: 'Robert Austin',
+    title: 'New user registered.'
+  },
+  {
+    meta: '11 Aug',
+    avatarAlt: 'message',
+    title: 'New message received 👋🏻',
+    avatarImg: '/images/avatars/5.png',
+    subtitle: 'You have 10 unread messages'
+  },
+  {
+    meta: '25 May',
+    title: 'Paypal',
+    avatarAlt: 'paypal',
+    subtitle: 'Received Payment',
+    avatarImg: '/images/misc/paypal.png'
+  },
+  {
+    meta: '19 Mar',
+    avatarAlt: 'order',
+    title: 'Revised Order 📦',
+    avatarImg: '/images/avatars/3.png',
+    subtitle: 'New order revised from John'
+  },
+  {
+    meta: '27 Dec',
+    avatarAlt: 'chart',
+    subtitle: '25 hrs ago',
+    avatarImg: '/images/misc/chart.png',
+    title: 'Finance report has been generated'
+  }
+]
 
 const AppBarContent = (props: Props) => {
   // ** Props
@@ -39,7 +86,7 @@ const AppBarContent = (props: Props) => {
       <Box className='actions-right' sx={{ display: 'flex', alignItems: 'center' }}>
         <LanguageDropdown settings={settings} saveSettings={saveSettings} />
         <ModeToggler settings={settings} saveSettings={saveSettings} />
-        <NotificationDropdown settings={settings} />
+        <NotificationDropdown settings={settings} notifications={notifications} />
         <UserDropdown settings={settings} />
       </Box>
     </Box>
