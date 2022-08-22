24f0426e77e67785ae92d42c0be7915263c7495a
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2022-08-22 10:31:07 +0530

    fix(calendar): bug fix
    Add a style so that checkbox doesn't take whole width & checkbox can be accessible only with its content

--- typescript-version/full-version/src/views/apps/calendar/SidebarLeft.tsx
+++ typescript-version/full-version/src/views/apps/calendar/SidebarLeft.tsx
@@ -70,6 +70,7 @@ const SidebarLeft = (props: SidebarLeftType) => {
             boxShadow: 'none',
             width: leftSidebarWidth,
             borderTopRightRadius: 0,
+            alignItems: 'flex-start',
             borderBottomRightRadius: 0,
             p: theme => theme.spacing(5),
             zIndex: mdAbove ? '2' : 'drawer',
