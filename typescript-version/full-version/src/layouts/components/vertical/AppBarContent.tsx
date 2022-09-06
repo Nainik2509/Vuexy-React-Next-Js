8b5de3501c394bdb039745b4f7415b01470b9942
ronaklalwaniii <ronak@pixinvent.com>
2022-09-06 14:03:47 +0530

    feat(Shortcuts) Adds Shortcuts Dropdown

--- typescript-version/full-version/src/layouts/components/vertical/AppBarContent.tsx
+++ typescript-version/full-version/src/layouts/components/vertical/AppBarContent.tsx
@@ -16,6 +16,7 @@ import LanguageDropdown from 'src/@core/layouts/components/shared-components/Lan
 import NotificationDropdown, {
   NotificationsType
 } from 'src/@core/layouts/components/shared-components/NotificationDropdown'
+import ShortcutsDropdown, { ShortcutsType } from 'src/@core/layouts/components/shared-components/ShortcutsDropdown'
 
 interface Props {
   hidden: boolean
@@ -69,6 +70,57 @@ const notifications: NotificationsType[] = [
   }
 ]
 
+const shortcuts: ShortcutsType[] = [
+  {
+    title: 'Calendar',
+    url: '/apps/calendar/',
+    subtitle: 'Appointments',
+    icon: 'mdi:calendar-month-outline'
+  },
+  {
+    title: 'Invoice App',
+    url: '/apps/invoice/list/',
+    subtitle: 'Manage Accounts',
+    icon: 'mdi:receipt-text-outline'
+  },
+  {
+    title: 'Users',
+    url: '/apps/user/list/',
+    subtitle: 'Manage Users',
+    icon: 'mdi:account-outline'
+  },
+  {
+    url: '/apps/roles/',
+    title: 'Role Management',
+    subtitle: 'Permissions',
+    icon: 'mdi:shield-check-outline'
+  },
+  {
+    url: '/',
+    title: 'Dashboard',
+    icon: 'mdi:chart-pie',
+    subtitle: 'User Dashboard'
+  },
+  {
+    title: 'Settings',
+    icon: 'mdi:cog-outline',
+    subtitle: 'Account Settings',
+    url: '/pages/account-settings/account/'
+  },
+  {
+    title: 'Help Center',
+    subtitle: 'FAQs & Articles',
+    icon: 'mdi:help-circle-outline',
+    url: '/pages/help-center/landing/'
+  },
+  {
+    title: 'Dialogs',
+    subtitle: 'Useful Dialogs',
+    icon: 'mdi:window-maximize',
+    url: '/pages/dialog-examples/'
+  }
+]
+
 const AppBarContent = (props: Props) => {
   // ** Props
   const { hidden, settings, saveSettings, toggleNavVisibility } = props
@@ -86,6 +138,7 @@ const AppBarContent = (props: Props) => {
       <Box className='actions-right' sx={{ display: 'flex', alignItems: 'center' }}>
         <LanguageDropdown settings={settings} saveSettings={saveSettings} />
         <ModeToggler settings={settings} saveSettings={saveSettings} />
+        <ShortcutsDropdown settings={settings} shortcuts={shortcuts} />
         <NotificationDropdown settings={settings} notifications={notifications} />
         <UserDropdown settings={settings} />
       </Box>
