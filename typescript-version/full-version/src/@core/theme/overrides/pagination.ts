5fc6972b22799419bac88f021d593b5b6a2de831
Neel Brahmakshatriya <64475785+neelbrahmakshatriya@users.noreply.github.com>
2022-12-05 12:15:42 +0530

    refactor(pagination): update pagination (#15)
    * refactor(pagination): update pagination
    
    * refactor(pagination): update pagination
    
    Update opacity of bg-color in outlined variant
    Removed hover bg-color to update opacity from MUI to 0.24

--- typescript-version/full-version/src/@core/theme/overrides/pagination.ts
+++ typescript-version/full-version/src/@core/theme/overrides/pagination.ts
@@ -8,23 +8,28 @@ const Pagination = (theme: Theme) => {
   return {
     MuiPaginationItem: {
       styleOverrides: {
+        root: {
+          '&:not(.MuiPaginationItem-outlined):not(.Mui-disabled)': {
+            transition: theme.transitions.create(['color', 'background-color', 'box-shadow'], {
+              duration: 250,
+              easing: 'cubic-bezier(0.4, 0, 0.2, 1)'
+            }),
+            '&.Mui-selected': {
+              boxShadow: theme.shadows[2]
+            }
+          }
+        },
         outlined: {
           borderColor: `rgba(${theme.palette.customColors.main}, 0.22)`
         },
         outlinedPrimary: {
           '&.Mui-selected': {
-            backgroundColor: hexToRGBA(theme.palette.primary.main, 0.12),
-            '&:hover': {
-              backgroundColor: `${hexToRGBA(theme.palette.primary.main, 0.2)} !important`
-            }
+            backgroundColor: hexToRGBA(theme.palette.primary.main, 0.16)
           }
         },
         outlinedSecondary: {
           '&.Mui-selected': {
-            backgroundColor: hexToRGBA(theme.palette.secondary.main, 0.12),
-            '&:hover': {
-              backgroundColor: `${hexToRGBA(theme.palette.secondary.main, 0.2)} !important`
-            }
+            backgroundColor: hexToRGBA(theme.palette.secondary.main, 0.16)
           }
         }
       }
