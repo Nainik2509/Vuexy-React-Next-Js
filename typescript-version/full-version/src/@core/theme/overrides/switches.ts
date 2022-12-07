46b5e2fca7ea0ec3dcc645cac7ddc3f3a54753cd
ronaklalwaniii <44769573+ronaklalwaniii@users.noreply.github.com>
2022-12-07 16:22:01 +0530

    refactor(switch): update switch (#24)
    Co-authored-by: neelbrahmakshatriya <brahma.neel.j@gmail.com>

--- typescript-version/full-version/src/@core/theme/overrides/switches.ts
+++ typescript-version/full-version/src/@core/theme/overrides/switches.ts
@@ -6,8 +6,103 @@ const Switch = (theme: Theme) => {
     MuiSwitch: {
       styleOverrides: {
         root: {
+          width: 54,
+          height: 42,
           '& .MuiSwitch-track': {
-            backgroundColor: `rgb(${theme.palette.customColors.main})`
+            width: 30,
+            height: 18,
+            opacity: 1,
+            borderRadius: 30,
+            backgroundColor: 'transparent',
+            border: `1px solid ${theme.palette.text.disabled}`,
+            transition: 'box-shadow 0.15s ease-in-out, background-color 0.15s ease-in-out'
+          }
+        },
+        switchBase: {
+          top: 5,
+          left: 6,
+          padding: `${theme.spacing(2.5)} !important`,
+          color: `rgba(${theme.palette.customColors.main}, 0.6)`,
+          transition: 'left 0.15s ease-in-out, transform 0.15s ease-in-out, color 0.15s ease-in-out',
+          '&:hover': {
+            backgroundColor: 'transparent !important'
+          },
+          '& .MuiSwitch-input': {
+            left: '-50%',
+            width: '250%'
+          },
+          '&.Mui-disabled': {
+            opacity: 0.4,
+            color: theme.palette.text.disabled,
+            '& + .MuiSwitch-track': {
+              opacity: 0.5
+            },
+            '&.Mui-checked': {
+              opacity: theme.palette.mode === 'dark' ? 0.5 : 0.9,
+              '& + .MuiSwitch-track': {
+                opacity: 0.3,
+                boxShadow: 'none'
+              }
+            }
+          },
+          '&.Mui-checked': {
+            left: -3,
+            color: `${theme.palette.common.white} !important`,
+            '& .MuiSwitch-input': {
+              left: '-100%'
+            },
+            '& + .MuiSwitch-track': {
+              opacity: 1,
+              borderColor: theme.palette.primary.main,
+              backgroundColor: theme.palette.primary.main,
+              boxShadow: `0 2px 3px 0 rgba(${
+                theme.palette.mode === 'light' ? theme.palette.customColors.main : '12, 16, 27'
+              }, 0.16)`
+            },
+            '&.MuiSwitch-colorSecondary + .MuiSwitch-track': {
+              borderColor: theme.palette.secondary.main,
+              backgroundColor: theme.palette.secondary.main
+            },
+            '&.MuiSwitch-colorSuccess + .MuiSwitch-track': {
+              borderColor: theme.palette.success.main,
+              backgroundColor: theme.palette.success.main
+            },
+            '&.MuiSwitch-colorError + .MuiSwitch-track': {
+              borderColor: theme.palette.error.main,
+              backgroundColor: theme.palette.error.main
+            },
+            '&.MuiSwitch-colorWarning + .MuiSwitch-track': {
+              borderColor: theme.palette.warning.main,
+              backgroundColor: theme.palette.warning.main
+            },
+            '&.MuiSwitch-colorInfo + .MuiSwitch-track': {
+              borderColor: theme.palette.info.main,
+              backgroundColor: theme.palette.info.main
+            }
+          }
+        },
+        thumb: {
+          width: 12,
+          height: 12,
+          boxShadow: 'none'
+        },
+        sizeSmall: {
+          width: 38,
+          height: 30,
+          '& .MuiSwitch-track': {
+            width: 24,
+            height: 16
+          },
+          '& .MuiSwitch-thumb': {
+            width: 10,
+            height: 10
+          },
+          '& .MuiSwitch-switchBase': {
+            top: 7,
+            left: 7,
+            '&.Mui-checked': {
+              left: -1
+            }
           }
         }
       }
