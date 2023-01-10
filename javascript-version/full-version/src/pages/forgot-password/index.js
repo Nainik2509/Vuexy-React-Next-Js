ae4827c5f9ac64d76a194409ec3c912ed2230344
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2023-01-10 13:38:34 +0530

    fix(multi): fix images and login url

--- javascript-version/full-version/src/pages/forgot-password/index.js
+++ javascript-version/full-version/src/pages/forgot-password/index.js
@@ -136,7 +136,7 @@ const ForgotPassword = () => {
                 Send reset link
               </Button>
               <Typography sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', '& svg': { mr: 1 } }}>
-                <LinkStyled href='/pages/auth/login-v2'>
+                <LinkStyled href='/login'>
                   <Icon fontSize='1.25rem' icon='tabler:chevron-left' />
                   <span>Back to login</span>
                 </LinkStyled>
